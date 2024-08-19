993a00f6fc1910f57b9df787e4a80b0c6cf81a04
Meagan Clarke <megclarke@g.ucla.edu>
2024-08-19 16:40:43 +0700

    changed put to post. Included stream_endpoint.py to test the streamin capabilities of query

---
+++ chatbot-project/stream_endpoint.py
@@ -0,0 +1,22 @@
+"""Stream the response from the FastAPI query endpoint."""
+
+import requests
+
+url = "http://127.0.0.1:8000/query"
+
+data = {
+    "question": (
+        "What are some common misconceptions about Henry VIII's wives, " "and what is the truth behind those myths?"
+    )
+}
+
+
+resp = requests.post(url, json=data, stream=True)
+
+# Process and print each line of the streamed response
+try:
+    for line in resp.iter_lines():
+        if line:
+            print(line.decode("utf-8"))
+finally:
+    resp.close()
